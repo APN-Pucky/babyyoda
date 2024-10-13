ffdd6b38b789da46e81e2c8111d54a03c1e7a9d7
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2024-10-13 12:37:40 +0200

    Add gzip support
    Closes: #15

--- src/babyyoda/grogu/write.py
+++ src/babyyoda/grogu/write.py
@@ -1,6 +1,16 @@
-def write(histograms, file_path: str):
+import gzip
+
+
+def open_file(file_path, gz=False):
+    if file_path.endswith(".gz") or file_path.endswith(".gzip") or gz:
+        return gzip.open(file_path, "wt")
+    else:
+        return open(file_path, "w")
+
+
+def write(histograms, file_path: str, gz=False):
     """Write multiple histograms to a file in YODA format."""
-    with open(file_path, "w") as f:
+    with open_file(file_path, gz=gz) as f:
         # if dict loop over values
         if isinstance(histograms, dict):
             histograms = histograms.values()
