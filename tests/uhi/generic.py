ec5e5ff266c46b9fee4448fa80fd0d1a8046a223
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2025-05-07 11:34:51 +0200

    test 2d too

--- tests/uhi/generic.py
+++ tests/uhi/generic.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
+import numpy as np
 import uhi.testing.indexing
 
 from babyyoda import grogu
 from babyyoda.histo1d import UHIHisto1D
+from babyyoda.histo2d import UHIHisto2D
 
 
-class TestAccess(uhi.testing.indexing.Indexing1D[UHIHisto1D]):
+class TestAccess1D(uhi.testing.indexing.Indexing1D[UHIHisto1D]):
     @staticmethod
     def make_histogram() -> UHIHisto1D:
         h = grogu.Histo1D(10, 0, 10, title="test")
@@ -15,4 +17,16 @@ class TestAccess(uhi.testing.indexing.Indexing1D[UHIHisto1D]):
             h.fill(i + 0.5, d)
         h.underflow().fill(3)
         h.overflow().fill(1)
+        return h
+
+
+class TestAccess2D(uhi.testing.indexing.Indexing2D[UHIHisto2D]):
+    @staticmethod
+    def make_histogram() -> UHIHisto2D:
+        h = grogu.Histo2D(2, 0, 2, 5, 0, 5, title="test")
+
+        x, y = np.mgrid[0:2, 0:5]
+        for i in range(2):
+            for j in range(5):
+                h.fill(x[i, j] + 0.5, y[i, j] + 0.5, i + j)
         return h
